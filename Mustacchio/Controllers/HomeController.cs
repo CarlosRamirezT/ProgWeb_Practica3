c1abc8aa3e4b85ac4dd6af544bdbf8e5df3d3507
Carlos Ramirez <38014393+CarlosRamirez@users.noreply.github.com>
2019-11-10 20:03:39 -0400

    Upload all the files from my pc

---
+++ Mustacchio/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mustacchio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: Gallery
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        // GET: Blog
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
